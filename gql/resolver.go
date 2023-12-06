5460fb4bf4c7082d7ae2818bdcfccfac5a192438
taro-28 <tarotaro0208@gmail.com>
2023-12-06 21:58:48 +0900

    Add GraphQL resolver and models

--- gql/resolver.go
+++ gql/resolver.go
@@ -1,11 +1,7 @@
-package graph
-
-import "github.com/taro-28/saas-sample-api/graph/model"
+package gql
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct {
-	todos []*model.Todo
-}
+type Resolver struct{}
