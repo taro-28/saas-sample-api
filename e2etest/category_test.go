7e1a27dafff962fbc137b9eef8ffcf4fe8dbf90b
taro-28 <tarotaro0208@gmail.com>
2023-12-24 16:09:41 +0900

    Refactor category deletion test in e2etest

--- e2etest/category_test.go
+++ e2etest/category_test.go
@@ -149,76 +149,67 @@ func TestE2E_Category(t *testing.T) {
 		}
 	})
 
-	// createRes, err := gqlClient.CreateCategory(ctx, "test")
-	// if err != nil {
-	// 	t.Fatalf("failed to create category: %v", err)
-	// }
+	t.Run("delete", func(t *testing.T) {
+		t.Parallel()
+		createRes, err := gqlClient.CreateCategory(ctx, gql.CreateCategoryInput{
+			Name: "test",
+		})
+		if err != nil {
+			t.Fatalf("failed to create category: %v", err)
+		}
+		id := createRes.CreateCategory.ID
 
-	// wantCreated := &CategoryFragment{
-	// 	ID:        createRes.CreateCategory.ID,
-	// 	Name:      "test",
-	// 	CreatedAt: int(testtime.Now().Unix()),
-	// 	Todos:     []*TodoFragment{},
-	// }
+		testCases := map[string]struct {
+			input   string
+			want    string
+			wantErr bool
+		}{
+			"ok:basic": {
+				input: id,
+				want:  id,
+			},
+			"ng: invalid id": {
+				input:   "invalid",
+				wantErr: true,
+			},
+		}
 
-	// if diff := cmp.Diff(wantCreated, &createRes.CreateCategory); diff != "" {
-	// 	t.Fatalf("mismatch (-want +got):\n%s", diff)
-	// }
+		for name, tc := range testCases {
+			name := name
+			tc := tc
+			t.Run(name, func(t *testing.T) {
+				t.Parallel()
+				deleteRes, err := gqlClient.DeleteCategory(ctx, tc.input)
+				if tc.wantErr {
+					if err == nil {
+						t.Fatalf("want error but got nil")
+					}
+					return
+				}
 
-	// categoriesRes, err := gqlClient.Categories(ctx)
-	// if err != nil {
-	// 	t.Fatalf("failed to get categories: %v", err)
-	// }
+				if err != nil {
+					t.Fatalf("failed to delete category: %v", err)
+				}
 
-	// wantList := &Categories{Categories: []*CategoryFragment{wantCreated}}
-	// if diff := cmp.Diff(wantList, categoriesRes); diff != "" {
-	// 	t.Fatalf("mismatch (-want +got):\n%s", diff)
-	// }
+				if deleteRes.DeleteCategory != tc.want {
+					t.Fatalf("want %s but got %s", tc.want, deleteRes.DeleteCategory)
+				}
 
-	// updateRes, err := gqlClient.UpdateCategory(ctx, gql.UpdateCategoryInput{
-	// 	ID:   wantCreated.ID,
-	// 	Name: "updated",
-	// })
-	// if err != nil {
-	// 	t.Fatalf("failed to update category name: %v", err)
-	// }
+				categoriesRes, err := gqlClient.Categories(ctx)
+				if err != nil {
+					t.Fatalf("failed to get categories: %v", err)
+				}
 
-	// wantUpdated := &CategoryFragment{
-	// 	ID:        updateRes.UpdateCategory.ID,
-	// 	Name:      "updated",
-	// 	CreatedAt: int(testtime.Now().Unix()),
-	// 	Todos:     []*TodoFragment{},
-	// }
+				for _, category := range categoriesRes.Categories {
+					if category.ID != createRes.CreateCategory.ID {
+						continue
+					}
 
-	// if diff := cmp.Diff(wantUpdated, &updateRes.UpdateCategory); diff != "" {
-	// 	t.Fatalf("mismatch (-want +got):\n%s", diff)
-	// }
-
-	// categoriesRes, err = gqlClient.Categories(ctx)
-	// if err != nil {
-	// 	t.Fatalf("failed to get categories: %v", err)
-	// }
-
-	// wantList = &Categories{Categories: []*CategoryFragment{wantUpdated}}
-	// if diff := cmp.Diff(wantList, categoriesRes); diff != "" {
-	// 	t.Fatalf("mismatch (-want +got):\n%s", diff)
-	// }
-
-	// deleteRes, err := gqlClient.DeleteCategory(ctx, wantUpdated.ID)
-	// if err != nil {
-	// 	t.Fatalf("failed to delete category: %v", err)
-	// }
-	// if deleteRes.DeleteCategory == "" {
-	// 	t.Fatalf("failed to delete category: %v", err)
-	// }
-
-	// categoriesRes, err = gqlClient.Categories(ctx)
-	// if err != nil {
-	// 	t.Fatalf("failed to get categories: %v", err)
-	// }
-
-	// wantList = &Categories{Categories: []*CategoryFragment{}}
-	// if diff := cmp.Diff(wantList, categoriesRes); diff != "" {
-	// 	t.Fatalf("mismatch (-want +got):\n%s", diff)
-	// }
+					if diff := cmp.Diff(tc.want, category.ID); diff != "" {
+						t.Fatalf("mismatch (-want +got):\n%s", diff)
+					}
+				}
+			})
+		}
+	})
 }
